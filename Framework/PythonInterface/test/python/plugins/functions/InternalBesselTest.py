16394d5271bb8731e7913097204b8c4e5f9ff68e
Wu <Chien.wu@stfc.ac.uk>
2019-09-20 14:26:17 +0100

    small change

--- Framework/PythonInterface/test/python/plugins/functions/InternalBesselTest.py
+++ Framework/PythonInterface/test/python/plugins/functions/InternalBesselTest.py
@@ -29,7 +29,7 @@ class InternalBesselTest(unittest.TestCase):
 			self.fail(msg.format(*[str(i) for i in (output, input, expected)]))
 
 	def test_do_fit(self):
-		do_a_fit(np.arange(0.1, 16, 0.2), 'InternalBessel',guess = dict(AlphaFast = 0.55, AlphaSlow = 0.55, Phi = 0.0, Nu = 2, LambdaFast = 1.05, LambdaSlow = 0.15, BetaF = 1.05, BetaS = 0.15),target = dict(AlphaFast = 0.5, AlphaSlow = 0.5, Phi = 0.0, Nu = 2, LambdaFast = 1.0, LambdaSlow = 0.1, BetaF = 1.0, BetaS = 0.1), atol = 0.01)
+		do_a_fit(np.arange(0.1, 16, 0.2), 'InternalBessel', guess = dict(AlphaFast = 0.55, AlphaSlow = 0.55, Phi = 0.0, Nu = 2, LambdaFast = 1.05, LambdaSlow = 0.15, BetaF = 1.05, BetaS = 0.15),target = dict(AlphaFast = 0.5, AlphaSlow = 0.5, Phi = 0.0, Nu = 2, LambdaFast = 1.0, LambdaSlow = 0.1, BetaF = 1.0, BetaS = 0.1), atol = 0.01)
 
 if __name__ == '__main__':
 	unittest.main()
